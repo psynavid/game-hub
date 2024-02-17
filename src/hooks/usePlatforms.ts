3cd62358b8de510f302f72198b68768df96c7415
Navid <psy.navid@yahoo.com>
2024-02-17 12:27:43 -0500

    refractoring-remove duplicates

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,23 @@
-import platforms from "../data/platforms";
+// import platforms from "../data/platforms";
 
-interface Platform {
+import apiClient from "../services/api-client";
+import { FetchQueryOptions, useQuery } from "@tanstack/react-query";
+
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    useQuery: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchQueryOptions<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staletime: 24 * 60 * 60 * 1000,
+    // initialData: platforms,
+  });
 
 export default usePlatforms;
